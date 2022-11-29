c573eb2d060d4c68e2e69628b5343d0cde1c7c7f
Andrew Stimson <andrew@applejuice.codes>
2022-11-29 15:07:30 -0800

    Exercise: Reverse Integers

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,11 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    const isPositive = n > 0
+    const number = parseInt(Math.abs(n).toString().split("").reverse().join(""))
 
-module.exports = reverseInt;
+    return isPositive ? number : n === 0 ? 0 : -number
+}
+
+module.exports = reverseInt
