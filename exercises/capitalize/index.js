2211220448c11dfc50d8897f831ea4a2cf5606e3
Andrew Stimson <andrew@applejuice.codes>
2022-12-02 22:44:36 -0800

    Exercise: Capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,16 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+    let capitalized = str[0].toUpperCase()
+    for (let i = 1; i < str.length; i++) {
+        if (str[i - 1].match(/\s/g)) {
+            capitalized += str[i].toUpperCase()
+        } else {
+            capitalized += str[i]
+        }
+    }
+    return capitalized
+}
 
-module.exports = capitalize;
+module.exports = capitalize
