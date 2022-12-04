ef912f76d821bc5e800a7e1901510e5bd699b03e
Andrew Stimson <andrew@applejuice.codes>
2022-12-04 12:29:12 -0800

    Exercise: Vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,8 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+    return str.split("").filter((char) => char.match(/[aeiou]/g)).length
+}
 
-module.exports = vowels;
+module.exports = vowels
