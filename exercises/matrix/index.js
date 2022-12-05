786226337ca8c23fd4d298c9f88b0ef4015091bf
Andrew Stimson <andrew@applejuice.codes>
2022-12-04 23:19:38 -0800

    Exercise: Matrix Spiral

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,57 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+    //create an array of arrays
+    //create n # of arrays
+    //create Counter
+    //create a startRow = 0, endRow = n-1
+    //create a startCol = 0, endCol = n-1
+    //create a for loop ending @ i <= n*n
+    //if i < n , increment startRow and set row[i] === counter
 
-module.exports = matrix;
+    const matrix = generateArray(n).map(() => generateArray(n))
+
+    /*
+     *   generate arrays for row and col
+     *   start count @ 0
+     */
+    const result = addRow(matrix, generateArray(n), generateArray(n), 0)
+
+    return result
+}
+function generateArray(n) {
+    //set array values to keys [0,1,2...]
+    return [...Array(n).keys()]
+}
+function addRow(matrix, row, col, count) {
+    col.forEach((i) => {
+        matrix[row[0]][i] = ++count
+    })
+
+    //this is essentially decrementing
+    row.shift()
+
+    //base case - keep repeating column generation if col still has length
+    if (col.length) {
+        return addCol(matrix, row, col.reverse(), count)
+    }
+
+    return matrix
+}
+
+function addCol(matrix, row, col, count) {
+    //loop through row
+    row.forEach((i) => {
+        matrix[i][col[0]] = ++count
+    })
+    col.shift()
+    //keep repeating row generation if row still has length
+    if (row.length) {
+        return addRow(matrix, row.reverse(), col, count)
+    }
+
+    return matrix
+}
+
+module.exports = matrix
