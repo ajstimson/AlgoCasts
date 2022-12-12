c7ae5bf0dcd749ba754bc5c04366d60c76c5c15d
Andrew Stimson <andrew@applejuice.codes>
2022-12-12 10:56:34 -0800

    Exercise: Linked List Node

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,13 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+    constructor(data, next = null) {
+        this.data = data
+        this.next = next
+    }
+}
 
 class LinkedList {}
 
-module.exports = { Node, LinkedList };
+module.exports = { Node, LinkedList }
