fbad577523fd0691d83fc7f6bc40262d206c71cf
Andrew Stimson <andrew@applejuice.codes>
2022-12-16 14:57:24 -0800

    Exercise: Events Library

--- exercises/events/index.js
+++ exercises/events/index.js
@@ -4,16 +4,31 @@
 // have methods 'on', 'trigger', and 'off'.
 
 class Events {
-  // Register an event handler
-  on(eventName, callback) {}
+    constructor(eventName) {
+        this.events = {}
+    }
+    // Register an event handler
+    on(eventName, callback) {
+        if (this.events[eventName]) {
+            this.events[eventName].push(callback)
+        } else {
+            this.events[eventName] = [callback]
+        }
+    }
 
-  // Trigger all callbacks associated
-  // with a given eventName
-  trigger(eventName) {}
+    // Trigger all callbacks associated
+    // with a given eventName
+    trigger(eventName) {
+        this.events[eventName].forEach((callback) => {
+            callback()
+        })
+    }
 
-  // Remove all event handlers associated
-  // with the given eventName
-  off(eventName) {}
+    // Remove all event handlers associated
+    // with the given eventName
+    off(eventName) {
+        this.events[eventName] = []
+    }
 }
 
-module.exports = Events;
+module.exports = Events
