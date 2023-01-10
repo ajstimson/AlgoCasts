b2ee2b806c791b2020b476a6cb6ddd2bd70ce014
Brux Feeblewitz <ajstimson@gmail.com>
2023-01-09 20:34:11 -0800

    Barret Course: LL push, pop, unshift

---
+++ scott-barrett-course/linked-list/index.js
@@ -0,0 +1,60 @@
+class Node {
+    constructor(value) {
+        this.value = value
+        this.next = null
+    }
+}
+
+class LinkedList {
+    constructor(value) {
+        const node = new Node(value)
+        this.head = node
+        this.tail = this.head
+        this.length = 1
+    }
+
+    push(value) {
+        const node = new Node(value)
+
+        if (!this.head) {
+            this.head = node
+            this.tail = node
+        } else {
+            this.tail.next = node
+            this.tail = node
+        }
+        this.length++
+    }
+
+    pop() {
+        if (this.length === 0) return undefined
+        let temp = this.head
+        let pre = this.head
+        while (temp.next) {
+            pre = temp
+            temp = temp.next
+        }
+        this.tail = pre
+        this.tail.next = null
+        this.length--
+        if (this.length === 0) {
+            this.head = null
+            this.tail = null
+        }
+        return temp
+    }
+
+    unshift(value) {
+        const node = new Node(value)
+        if (!this.head) {
+            this.head = node
+            this.tail = node
+        } else {
+            node.next = this.head
+            this.head = node
+        }
+
+        this.length++
+        return this
+    }
+}
