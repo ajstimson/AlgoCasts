a9d2d909612cd5bbfccd7fd0fbef10415a270716
Brux Feeblewitz <ajstimson@gmail.com>
2023-01-13 08:35:19 -0800

    Barret Course: DLL push

--- scott-barrett-course/doubly-linked-list/index.js
+++ scott-barrett-course/doubly-linked-list/index.js
@@ -14,5 +14,19 @@ class DoublyLinkedList {
         this.length = 1
     }
 
-    
+    push(value){
+        const newNode = new Node(value)
+
+        if(!this.head){
+            this.head = newNode
+            this.tail = newNode
+        } else {
+            this.tail.next = newNode
+            newNode.prev = this.tail
+            this.tail = newNode
+        }
+        this.length++
+        return this
+    }
+
 }
