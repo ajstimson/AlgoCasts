7157954e80922abdd1cdb947e1b7ab583244234e
Andrew Stimson <andrew@applejuice.codes>
2023-01-13 16:27:17 -0800

    Barret Course: DLL unshift & shift

--- scott-barrett-course/doubly-linked-list/index.js
+++ scott-barrett-course/doubly-linked-list/index.js
@@ -14,10 +14,10 @@ class DoublyLinkedList {
         this.length = 1
     }
 
-    push(value){
+    push(value) {
         const newNode = new Node(value)
 
-        if(!this.head){
+        if (!this.head) {
             this.head = newNode
             this.tail = newNode
         } else {
@@ -29,4 +29,50 @@ class DoublyLinkedList {
         return this
     }
 
+    pop() {
+        if (!this.length === 0) return undefined
+
+        let node = this.tail
+
+        if (this.length === 1) {
+            this.head = null
+            this.tail = null
+        } else {
+            this.tail = this.tail.prev
+            this.tail.next = null
+            node.prev = null
+        }
+        this.length--
+
+        return node
+    }
+
+    unshift(value) {
+        let newNode = new Node(value)
+        if (this.length === 0) {
+            this.head = newNode
+            this.tail = newNode
+        } else {
+            newNode.next = this.head
+            this.head.prev = newNode
+            this.head = newNode
+        }
+        this.length++
+        return this
+    }
+
+    shift() {
+        if (this.length === 0) return undefined
+        const node = this.head
+        if (this.length === 1) {
+            this.head = null
+            this.tail = null
+        } else {
+            this.head = this.head.next
+            this.head.prev = null
+            node.next = null
+        }
+        this.length--
+        return node
+    }
 }
